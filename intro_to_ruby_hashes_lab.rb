9e565e9c02c0af26801ccccd1b805fa8666a6bab
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-01 18:57:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,8 @@ def monopoly_with_third_tier
 	  :railroads => {pieces: 4, names:{
 	    reading_railroad: {},
 	    pennsylvania_railroad: {},
-	    b_n_o
+	    b_and_o_railroad: {},
+	    shortline_railroad: {}
 	  }, rent_in_dollars:{
 	    one_piece_owned: 25,
 	    two_pieces_owned: 50,
